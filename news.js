dea9a9c367c7b8dc918dc31e07789ca37c8b3948
Seam Park <run128@naver.com>
2024-02-20 19:57:34 +0900

    API 호출하기

---
+++ news.js
@@ -0,0 +1,12 @@
+let news = [];
+const getNewsAPI = async() => {
+    const url = new URL(`http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines`);
+    const response = await fetch(url);
+    const data = await response.json();
+    
+    news = data.articles;
+
+    console.log(news)
+}
+
+getNewsAPI();
