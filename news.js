414c91738a5dadfbc7f7c1bf1a1906e3c0af0aa6
Seam Park <run128@naver.com>
2024-02-20 20:04:33 +0900

    주소변경

--- news.js
+++ news.js
@@ -1,6 +1,8 @@
 let news = [];
+let myNetlify = "https://noona-study-site.netlify.app";
+let noonaNetlify = "http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com";
 const getNewsAPI = async() => {
-    const url = new URL(`http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines`);
+    const url = new URL(`${myNetlify}/top-headlines`);
     const response = await fetch(url);
     const data = await response.json();
     
