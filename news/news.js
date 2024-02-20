f8bedbaeb926d33d3688e68ed8e926b604b814c5
Seam Park <run128@naver.com>
2024-02-21 02:41:35 +0900

    카테고리 리스트  추가

--- news/news.js
+++ news/news.js
@@ -1,65 +1,75 @@
 let newsList = [];
 let myNetlify = "https://noona-study-site.netlify.app";
 let dateMoment = moment();
-
+let menu = document.querySelector(".menu");
+let category = '';
+let categoryTxt = '';
 
 // let noonaNetlify = "http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com";
 const getNewsAPI = async() => {
-    const url = new URL(`${myNetlify}/top-headlines`);
+    const url = new URL(`${myNetlify}/top-headlines${category}`);
     const response = await fetch(url);
     const data = await response.json();
     
     newsList = data.articles;
 
-    render();
-    
+    console.log(newsList)
 
+    render();
 }
 
-// function errorCheck(){
-//     let imgAll = document.querySelectorAll(".news .img img");
+menu.addEventListener('click',categoryList);
 
-//     for(i=0; i<imgAll.length;i++){
-//         console.log(imgAll[i].height)
-//         console.log(imgAll[i].width)
-//         if(imgAll[i].height === 0) {
-//             imgAll[i].setAttribute('src','ico_no_pictures.png')
-//         }
-//     }
- 
-// }
+function categoryList(e) {
+    e.preventDefault()
+    let thisTarget = e.target;
+    let thisTag = thisTarget.tagName;
+
+    if(thisTag == 'A') {
+        let categoryHref = thisTarget.getAttribute('href');
+        category = `?category=${categoryHref}`;
+        categoryTxt = thisTarget.innerText;
+
+        getNewsAPI();
+    }
+}
 
 const render = () => {
+    let listLength = newsList.length;
+    let newHTML = ``;
 
-    console.log(dateMoment)
-    const newHTML = newsList.map(function(news) {
-        let description = news.description;
-        let resultDescription = '';
-        if (description.length >= 200) {
-            resultDescription = description.substr(0, 200 - 3) + '...';
-        } else {
-            resultDescription = description;
-        }
-      
-        return (`<li>
-            <div class="news">
-                <div class="img">
-                    <img src="${news.urlToImage==''?'이미지 없음':news.urlToImage}" alt="" />
-                </div>
-                <div class="txt">
-                    <div class="cont">
-                        <strong class="title">${news.title}</strong>
-                        <p>${resultDescription==''?'내용 없음':resultDescription}</p>
+    if(listLength > 0){
+        newHTML = newsList.map(function(news) {
+            let description = news.description;
+            let resultDescription = '';
+            if (description&&description.length >= 200) {
+                resultDescription = description.substr(0, 200 - 3) + '...';
+            } else {
+                resultDescription = description;
+            }
+          
+            return (`<li>
+                <div class="news">
+                    <div class="img">
+                        <img src="${news.urlToImage==''?'이미지 없음':news.urlToImage}" alt="" />
                     </div>
-                    <div class="source">
-                        <span>${news.source.name==''?'출처 없음':news.source.name}</span>
-                        <span>${moment(news.publishedAt).fromNow()}</span>
+                    <div class="txt">
+                        <div class="cont">
+                            <strong class="title">${news.title}</strong>
+                            <p>${resultDescription==''?'내용 없음':resultDescription}</p>
+                        </div>
+                        <div class="source">
+                            <span>${news.source.name==''?'출처 없음':news.source.name}</span>
+                            <span>${moment(news.publishedAt).fromNow()}</span>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </li>
-        `
-        )} ).join('');
+            </li>
+            `
+            )} ).join('');
+    } else {
+        newHTML = `<li class="no_data"><b>${categoryTxt}</b> 관련 뉴스가 없습니다.</li>`;
+    }
 
     document.getElementById('news_list').innerHTML = newHTML;
 }
